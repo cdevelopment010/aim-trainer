e7dca2d902861c82b9cb8c548c31b239b7156aea
cdevelopment010 <cdevelopment010@gmail.com>
2023-04-25 13:42:24 +0100

    Feat: Homepage game list
    Links to different game modes.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,8 +14,13 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        Main page...
+      <main className={``}>
+        <h1>Aim trainer</h1>
+        <p>Select a game from the list below.</p>
+        <div>
+         <Link href="/initialgame">Initial Game!</Link>
+         <p>This is the first iteration of the aim trainer. Try to hit as many targets as possible within the time limit</p>
+        </div>
       </main>
     </>
   )
