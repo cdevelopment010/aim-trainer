5bfa6fafc7ce55b20b6ffe667d2ad65b08a2860e
Craig <cdevelopment010@gmail.com>
2023-08-30 19:23:17 +0100

    Fix: Overlapping score with accuracy
    Resolves #10

--- src/pages/grid-shot.js
+++ src/pages/grid-shot.js
@@ -244,7 +244,7 @@ const precisionGame = () => {
           <Nav />
           <div className={`d-flex justify-content-center align-center ${style['game-nav']} gap-10` }>
             <div className={`text-center-div text-colour-white-60 ${style['w-4']} ${style.timer}`}>{Math.round(timer*1)/1000}</div>
-            <div className={`text-center-div text-colour-2 ${style['w-7']} ${style.counter}`}>{counter}</div>
+            <div className={`text-center-div text-colour-2 ${style['min-w-7']} ${style.counter}`}>{counter}</div>
             <div className={`text-center-div text-colour-white-60 ${style['w-4']} ${style.accuracy}`}>{ new Intl.NumberFormat(undefined, {style:'percent'}).format(accuracy) }</div>
           </div>
           <canvas id="game-container" width={windowWidth} height={windowHeight} onClick={addMouseClicks}></canvas>
--- src/pages/tracking-game.js
+++ src/pages/tracking-game.js
@@ -216,7 +216,7 @@ const TrackingGame = () => {
           <Nav />
           <div className={`d-flex justify-content-center align-center ${style['game-nav']} gap-10` }>
             <div className={`text-center-div text-colour-white-60 ${style['w-4']} ${style.timer}`}>{Math.round(timer*1)/1000}</div>
-            <div className={`text-center-div text-colour-2 ${style['w-7']} ${style.counter}`}>{counter}</div>
+            <div className={`text-center-div text-colour-2 ${style['min-w-7']} ${style.counter}`}>{counter}</div>
             <div className={`text-center-div text-colour-white-60 ${style['w-4']} ${style.accuracy}`}>{ new Intl.NumberFormat(undefined, {style:'percent'}).format(accuracy) }</div>
           </div>
           
